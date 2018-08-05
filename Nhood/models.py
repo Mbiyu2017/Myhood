66947af0d80a72bb4bd0f24cbb298af43a3464e3
Ben_Mbiyu <benedictmbiyuthangwa@gmail.com>
2018-08-05 13:59:37 +0300

    Create models and propeties for userprofile, neighbourhood and business

--- Nhood/models.py
+++ Nhood/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=50)
+    occupants = models.IntegerField()
+    admin = models.CharField(max_length=30)
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    hood = models.ForeignKey(Neighbourhood)
+    email = models.EmailField(max_length=254)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Userprofile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(Userprofile)
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    email = models.EmailField(max_length=254)
